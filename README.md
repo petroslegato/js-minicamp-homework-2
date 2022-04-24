ecbb8b25ba8fc58cde7af5d34737d570bfc2dd93
Petros <petroslegato@gmail.com>
2022-04-23 20:10:27 -0600

    homework 2

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 ---
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
 		
-	* `for`
-	* `&&`, `||`, `!`
-	* Arrays
-	* Git
-	* GitHub
+	* `for` A "For" Loop is used to repeat a specific block of code a known number of times. For example, if we want to check the grade of every student in the class, we loop from 1 to that number. When the number of times is not known before hand, we use a "While" loop. 
+	* `&&`, `||`, `!` The logical AND (&&) operator (logical conjunction) for a set of boolean operands will be true if and only if all the operands are true. Otherwise it will be false — The logical AND (&&) operator (logical conjunction) for a set of boolean operands will be true if and only if all the operands are true. Otherwise it will be false — "!" The logical complement operator, also known as the NOT operator, is represented by an exclamation mark '! '. This operator changes true values to false and false to true.
+	* Arrays Arrays are "lists" of related values. Every value in the array is usually of the exact same type and only differentiated by the position in the array. For example, all the quiz scores for a test could be stored in an array with the single variable name: quiz_scores. The first students quiz score would be found in the first "bucket" of the array, the second students score in the "second" bucket, etc. 
+	* Git Git is a DevOps tool used for source code management. It is a free and open-source version control system used to handle small to very large projects efficiently. Git is used to tracking changes in the source code, enabling multiple developers to work together on non-linear development.
+	* GitHub GitHub is a for-profit company that offers a cloud-based Git repository hosting service. Essentially, it makes it a lot easier for individuals and teams to use Git for version control and collaboration.
 
 
 2. Install git.  https://git-scm.com/downloads
--- exercises.js
+++ exercises.js
@@ -1,12 +1,22 @@
 // Do not change any of the function names
 
 function getBiggest(x, y) {
-  //testing shh terminal connection x222
+  if (x>y) { 
+    return x;
+  } else { 
+    return y;}
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
 }
 
 function greeting(language) {
+  if (language === 'German') {
+    return 'Guten Tag!';
+  } else if (language === 'Spanish') {
+    return 'Hola!';
+  } else {
+    return 'Hello!';
+  }
   // return a greeting for three different languages:
   // language: 'German' -> 'Guten Tag!'
   // language: 'English' -> 'Hello!'
@@ -15,15 +25,30 @@ function greeting(language) {
 }
 
 function isTenOrFive(num) {
+  if (num === 10 || num === 5) {
+    return true;
+  } else {
+    return false;
+  }
   // return true if num is 10 or 5
   // otherwise return false
 }
 
 function isInRange(num) {
+  if (num <50 && num > 20) {
+    return true;
+  } else {
+    return false;
+  }
   // return true if num is less than 50 and greater than 20
 }
 
 function isInteger(num) {
+  if (Number.isInteger(num)) {
+    return true;
+  } else {
+    return false;
+  }
   // return true if num is an integer
   // 0.8 -> false
   // 1 -> true
@@ -33,6 +58,15 @@ function isInteger(num) {
 }
 
 function fizzBuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else {
+    return num;
+  }
   // if num is divisible by 3 return 'fizz'
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
@@ -40,6 +74,15 @@ function fizzBuzz(num) {
 }
 
 function isPrime(num) {
+  if (num === 1 || num === 0) {
+    return false;
+  } else if (num > 1) {
+    for (var i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    } return true;
+  }
   // return true if num is prime.
   // otherwise return false
   // hint: a prime number is only evenly divisible by itself and 1
@@ -48,57 +91,86 @@ function isPrime(num) {
 }
 
 function returnFirst(arr) {
+  return arr[0];
   // return the first item from the array
 }
 
 function returnLast(arr) {
+  return arr[arr.length -1];
   // return the last item of the array
 }
 
 function getArrayLength(arr) {
+  return arr.length;
   // return the length of the array
 }
 
 function incrementByOne(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    arr[i]++;
+  } return arr;
   // arr is an array of integers  
   // increase each integer by one
   // return the array
 }
 
 function addItemToArray(arr, item) {
+  arr.push(item);
+  return arr;
   // add the item to the end of the array
   // return the array
 }
 
 function addItemToFront(arr, item) {
+  arr.unshift(item);
+  return arr;
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
 }
 
 function wordsToSentence(words) {
+  var together = words.join(' ');
+  return together;
   // words is an array of strings
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
 }
 
-function contains(arr, item) {
+function contains(arr, item) { 
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      return true;
+    }
+  } return false;
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
 }
 
 function addNumbers(numbers) {
+  var sumVal = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    sumVal += numbers[i];
+  } return sumVal;
   // numbers is an array of integers.
   // add all of the integers and return the value
 }
 
 function averageTestScore(testScores) {
+  var totVal = 0;
+  var avgVal = 0;
+  for (var i = 0; i < testScores.length; i++) {
+    totVal += testScores[i];
+  } avgVal = totVal / testScores.length;
+  return avgVal;
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
 }
 
 function largestNumber(numbers) {
+  var largest = Math.max.apply(Math, numbers);
+  return largest;
   // numbers is an array of integers
   // return the largest integer
 }
